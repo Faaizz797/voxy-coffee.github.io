e6c907d777d87e309dfe818a985ba3975d5516ac
Faaizz797 <faaizz.797@gmail.com>
2024-01-15 14:06:14 +0700

    first-commit

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+// navbarfixed
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const navbarFixed = header.offsetTop;
+
+    if (window.pageYOffset){
+        header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+// button humburger
+const humburger = document.querySelector('#humburger');
+const navMenu = document.querySelector('#nav-menu');
+
+humburger.addEventListener('click',function(){
+    humburger.classList.toggle('active');
+    navMenu.classList.toggle('hidden');
+})
